410030acc2764a97415b3e34f998a2ca5772e647
waynian <waynian@foxmail.com>
2018-12-17 20:28:30 +0800

    style 首页修改

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,14 +1,40 @@
 <template>
-  <div>
-    布局
-    <router-view />
-  </div>
+  <v-app light>
+    <v-toolbar class="white">
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <router-view />
+      <v-footer class="blue darken-2" height="auto">
+        <v-layout row wrap align-center>
+          <v-flex xs12>
+            <div class="white--text ml-3">
+              Made with
+              <v-icon class="red--text">favorite</v-icon>
+              by <a class="white--text" href="#" target="_blank">waynian</a>
+            </div>
+          </v-flex>
+          <v-flex xs12>
+            <div class="white--text ml-3">
+              备案号：
+              <a
+                class="white--text"
+                href="http://www.miibeian.gov.cn"
+                target="_blank"
+                >苏ICP备18061251号</a
+              >
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-footer>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 export default {
   data: () => ({
-    lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`
+    title: "Your Logo"
   })
 };
 </script>
