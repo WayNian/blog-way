76cb79da573dac734617fdecbfb79005f8420a52
waynian <waynian@foxmail.com>
2022-07-29 14:21:43 +0800

    docs: 增加链接

--- docs/.vuepress/configs/links.ts
+++ docs/.vuepress/configs/links.ts
@@ -2,6 +2,18 @@ import type { LinkGroup } from "vuepress-theme-way";
 
 export const linkGroup: LinkGroup[] = [
   {
+    title: "前端学习",
+    children: [
+     
+      {
+        url: "https://docschina.org/",
+        avatar: "https://docschina.org/favicon.ico",
+        name: "印记中文",
+        description: "印记中文，深入挖掘国外前端新领域，为国内 Web 前端开发人员提供优质文档!!",
+      },
+    ],
+  },
+  {
     title: "JS框架",
     children: [
       {
